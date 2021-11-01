64f5721fd745dd29f93d88f5f3f6f99ea870f081
Gavin <92048502+gsmoran1020@users.noreply.github.com>
2021-11-01 13:58:01 -0400

    nc

--- django_chat/django_chat/settings.py
+++ django_chat/django_chat/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dq%hqfxb7z7(*wp79mj=p=*_1#*qzslg*7l=7wm2!w3)u*8^8='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000']
 
