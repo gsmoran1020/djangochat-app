a0f102ba88f062a495f7fe8c0e6eeba260f9fa75
Gavin <92048502+gsmoran1020@users.noreply.github.com>
2021-11-01 13:30:04 -0400

    nc

--- django_chat/django_chat/settings.py
+++ django_chat/django_chat/settings.py
@@ -87,7 +87,7 @@ ASGI_APPLICATION = 'django_chat.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
