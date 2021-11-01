1ac5d074ac054744c2ecf5107dd285370b744ed3
Gavin <92048502+gsmoran1020@users.noreply.github.com>
2021-11-01 13:35:31 -0400

    nc

--- django_chat/django_chat/settings.py
+++ django_chat/django_chat/settings.py
@@ -87,7 +87,7 @@ ASGI_APPLICATION = 'django_chat.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -132,9 +132,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
